0799a18af21e130150507e0d8160ac8e84651bc8
Sonu Kumar <sonu82256@gmail.com>
2024-07-22 13:16:17 +0530

    Day3

---
+++ day3/level1.js
@@ -0,0 +1,7 @@
+let firstName = 'sonu'; let lastName = 'kumar';let country = 'India'; let city = 'Ranchi'; let age = '24'; let isMarried = false; console.log(typeof(firstName), typeof(lastName), typeof(country), typeof(city), typeof(age), typeof(isMarried));
+console.log(typeof(10)==typeof('10'))
+console.log(parseInt('9.8')==10)
+console.log(10==10, 10===parseInt('10'), true == 1, 10 === '10', true === 1, 10 === parseFloat('9.8'))
+console.log(4>3, 4>=3, 4<3, 4<=3, 4==4,4===4,4!=4,4!==4, 4!='4',4=='4',4==='4', 'python'.length, 'jargon'.length)
+console.log(4 > 3 && 10 < 12, 4 > 3 && 10 > 12, 4 > 3 || 10 < 12, 4 > 3 || 10 > 12, !(4 > 3), !(4 < 3), !(false), !(4 > 3 && 10 < 12), !(4 > 3 && 10 > 12), !(4 === '4'));
+const date = new Date(); console.log(date.getFullYear(), date.getMonth(), date.getDate(), date.getDay(), date.getHours(), date.getMinutes(), date.getTime());
---
+++ day3/level2.js
@@ -0,0 +1,15 @@
+// let length = prompt("Enter length"); let height = prompt("Enter height"); alert(`The area of the triangel is ${length*height*0.5}`);
+// let a = prompt("Enter side a:"); let b = prompt('Enter side b:'); let c = prompt('Enter side c:'); alert(`The perimeter od the traingle is ${+a + +b+ +c}`);
+// const [length, width] = [parseFloat(prompt("Enter the length of the rectangle:")), parseFloat(prompt("Enter the width of the rectangle:"))]; alert(`Area: ${length * width}, Perimeter: ${2 * (length + width)}`);
+// const [pi, r] = [3.14,prompt('Enter the radius:')]; alert(`Area of circle = ${pi*r*r}`)
+const [x,y] = [4,6]; console.log(2*x -2);
+const [x1,y1,x2,y2] = [2,2,6,10]; console.log((y2-y1)/(x2-x1))
+
+console.log(x**x + 6*x + 9);
+
+console.log('sonu'.length > 7 ? 'long': 'sort')
+
+const [myage, yourage] = [24, 10]; console.log(`I am ${myage-yourage} older than you`)
+const [yob,date1] = [2000,new Date]; console.log(date1.getFullYear()-yob > 18 ? `you are ${date1.getFullYear()-yob}. You are old enough to drive` : `You are ${date1.getFullYear()-yob}. You will be allowed to drive after ${18-(date1.getFullYear()-yob)} years`)
+const noyyl = 100; console.log(100*365*24*60*60)
+console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`)
